bad38f1b67e4755fe44cd2d1d2cb426545fd48ea
Practice <hcxiaoling@163.com>
2024-03-27 10:59:03 +0800

    test

---
+++ HomeWork_2024_3_26/2024_3_26 HomeWork/HomeWork.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = Add(a, b);
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+
+
+//int num = 10;
+//int main()
+//{
+//	int num = 1;
+//	printf("num = %d\n", num);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
