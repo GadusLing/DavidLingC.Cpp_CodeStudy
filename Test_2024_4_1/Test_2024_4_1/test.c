6c198ff2c981f2afb3449eb1fb6188165efbfae9
Practice <hcxiaoling@163.com>
2024-04-23 14:57:07 +0800

    2024_4_23学习资料

--- Test_2024_4_1/Test_2024_4_1/test.c
+++ Test_2024_4_1/Test_2024_4_1/test.c
@@ -145,7 +145,31 @@
 //	return 0;
 //}
 
+//学生
+struct Stu
+{
+	//结构体的成员
+	char name[20];
+	int age;
+	char sex[10];
+	char tele[12];
+};
+void print(struct Stu* ps)
+{
+	printf("%s %d %s %s\n", (*ps).name, (*ps).age, (*ps).sex, (*ps).tele);
+	//->
+	//结构体指针变量->成员名
+	printf("%s %d %s %s\n", ps->name, ps->age, ps->sex, ps->tele);
+}
+int main()
+{
+	struct Stu s = {"zhangsan",20,"nan","15601525568"};
 
+	//结构体对象.成员名
+	//printf("%s %d %s %s\n", s.name, s.age, s.sex, s.tele);//要一一对应，不要搞错位置了
+	print(&s);
+	return 0;
+}
 
 
 
