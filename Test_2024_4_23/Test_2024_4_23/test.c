7a2e5282527b7bec55930d8e029af7be9b826255
Practice <hcxiaoling@163.com>
2024-05-08 21:06:20 +0800

    2024_5_8学习资料

---
+++ Test_2024_4_23/Test_2024_4_23/test.c
@@ -0,0 +1,169 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+//int main()
+//{
+//	int a = 10;
+//	if (a == 3)
+//		printf("%s\n", "hehe");
+//
+//	int age = 20;
+//	if (age > 18)
+//	{
+//		printf("未成年\n");
+//		printf("不能饮酒\n");
+//	}
+//	else
+//	{
+//        printf("成年\n");
+//	}
+//
+//	return 0;
+//}
+	//int age = 10;
+	//scanf("%d", &age);
+	//if (age < 18)
+	//{
+	//	printf("青少年\n");
+	//}
+	//else if (age >= 18 && age < 28)
+	//{
+	//	printf("青年\n");
+	//}
+	//else if (age >= 28 && age < 40)
+	//{
+	//	printf("中年\n");
+	//}
+	//else if (age >= 40 && age < 60)
+	//{
+	//	printf("壮年\n");
+	//}
+	//else if (age >= 60 && age <= 100)
+	//{
+	//	printf("老年\n");
+	//}
+	//else
+	//{
+	//	printf("老寿星\n");
+	//}
+
+	//return 0;
+
+	//int main();
+	//{
+	//	int age = 20;
+	//	if (age < 18)
+	//		printf("未成年\n");
+	//	else
+	//	{
+	//		printf("成年\n");
+	//		printf("打游戏\n");
+	//	}
+
+	//		return 0;
+	//}
+
+	//int main();
+	//{
+	//	int a = 0;
+	//	int b = 2;
+	//	if (a == 1)
+	//	{
+	//		if (b == 2)
+	//			printf("hehe\n");
+	//		else
+	//			printf("haha\n");
+	//	}
+	//	return 0;
+	//}
+
+	//int main();
+	//{
+	//	int a = 0;
+	//	int b = 2;
+	//	if (a == 1)
+	//	{
+	//		if (b == 2)
+	//			printf("hehe\n");
+	//	}
+	//	else
+	//		printf("haha\n");
+	//	
+	//	return 0;
+	//}
+
+	//1.同时还要注意：变量的命名最好是有意义的，规范的
+	//int main;
+	//{
+	//	//char firstname[20] = { 0 };
+	//	//char first_name[20] = { 0 };//下划线区分
+	//	//char FirstName[20] = { 0 };//驼峰
+
+	//	int a = 10;
+	//	int a=10;//密集，不舒服
+
+    //  return 0;
+	//}
+
+//int test()
+//{
+//	int a = 4;
+//	if (a == 3)
+//		return 1;
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int r = test();
+//	printf("%d\n", r);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 3;
+//		if (5 == num)//if (num == 5)   //在判断数值而非赋值的情况下，常量放左值的写法情况可以避免遗失等号的错误
+//			printf("hehe\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	//判断
+//	if (num % 2 == 1)
+//		printf("奇数\n");
+//	else
+//		printf("非奇数\n");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 1;
+//	while (num >= 1 & num <= 100)
+//	{
+//		if (num % 2 == 1)
+//			printf("%d ", num);
+//		num++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 1;
+//	while (num >= 1 & num <= 100)
+//	{
+//		if (num % 2 == 1)
+//			printf("%d ", num);
+//		num+=2;//+2一样也是奇数，从数理角度避免了枚举过程，减少了损耗
+//	}
+//	return 0;
+//}
+
