7e4bb47b13693d74ab71304ab2be9208fe9fa909
Practice <hcxiaoling@163.com>
2024-06-18 17:05:33 +0800

    2024_6_18学习资料

--- Test_2024_6_17 P_49/Test_2024_6_17 P_49/test.c
+++ Test_2024_6_17 P_49/Test_2024_6_17 P_49/test.c
@@ -1,2 +1,167 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		if (i = 5)//只有一个等号！！！怎么不敏感呢？？是赋值，i被赋值成5了，反复的被赋值5，不断打印
+//			printf("%d", i);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	switch (n)
+//	{
+//	case 1:
+//		printf("1\n");
+//		break;
+//	case 2:
+//		printf("1\n");
+//		break;
+//	default:
+//		printf("呵呵\n");
+//		break;
+//
+//	}
+//
+//	return 0;
+//
+//}
+
+
+//int func(int a)
+//{
+//	int b;
+//	switch (a)
+//	{
+//	case 1:b = 30;
+//	case 2:b = 20;
+//	case 3:b = 16;
+//	default:b = 0;
+//	}
+//
+//	return b;
+//}
+
+//int main()
+//{
+//	int x = 3;
+//	int y = 3;
+//	switch (x % 2)
+//	{
+//	case 1:
+//		switch (y)
+//		{
+//		case 0:
+//			printf("first");
+//		case 1:
+//			printf("second");
+//			break;
+//		default: printf("hello");
+//		}
+//	case 2:
+//		printf("third");
+//	}
+//	return 0;
+//}
+
+
+//写一个代码打印1-100之间所有3的倍数的数字
+//void swap(int *px, int* py)
+//{
+//	int tmp = *px;
+//	*px = *py;
+//	*py = tmp;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//
+//	scanf("%d %d %d", &a, &b, &c);
+//	int tmp = 0;
+//	if (a < b)
+//	{
+//		swap(&a, &b);
+//	}
+//	if (a < c)
+//	{
+//		swap(&a, &c);
+//	}
+//	if (b < c)
+//	{
+//		swap(&b, &c);
+//	}
+//	printf("%d %d %d\n", a, b, c);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 3; i <= 100; i+=3)
+//	{
+//		printf("%d ", i);
+//	}
+//
+//	return 0;
+//}
+
+
+//给定两个数，求这两个数的最大公约数  欧几里得算法-辗转相除法
+
+//先来枚举法
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	
+//	int min = (a < b) ? a : b;
+//	int m = min;
+//
+//	while (1)
+//	{
+//		if (a % m == 0 && b % m == 0)
+//		{
+//			break;
+//		}
+//		m--;
+//	}
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+
+//辗转相除
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//    int c = 0;
+//	scanf("%d %d", &a, &b);
+//
+//	while (c = a % b)//没有写条件，因为0为假，非0为真，简洁
+//	{
+//		a = b;
+//		b = c;
+//	}
+//	printf("%d\n", b);
+//
+//	return 0;
+//}
+
+
+
+
