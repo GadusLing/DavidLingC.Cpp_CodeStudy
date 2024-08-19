2c599a779283d69fd0261fc0ee5caabc0d3eba26
Practice <hcxiaoling@163.com>
2024-08-20 07:56:09 +0800

    2024_8_19日之前学习资料

--- Test_2024_8_15 P_90/Test_2024_8_15 P_90/test.c
+++ Test_2024_8_15 P_90/Test_2024_8_15 P_90/test.c
@@ -55,5 +55,73 @@
 //	return 0;
 //}
 
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5 };
+//	short* p = (short*)arr;
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		*(p + i) = 0;
+//	}
+//
+//	for (i = 0; i < 5; i++) 
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	unsigned long pulArray[] = { 6,7,8,9,10 };
+//	unsigned long* pulptr;
+//	pulptr = pulArray;
+//	*(pulptr + 3) += 3;
+//	printf("%d, %d\n", *pulptr, *(pulptr + 3));
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>  
+
+int gcd(int a, int b) //使用欧几里得算法求最大公约数的函数  
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() 
+{
+    int num1, num2;
+
+    printf("请输入两个数（用空格分隔）: ");
+    scanf("%d %d", &num1, &num2);
+
+    //调用gcd函数并打印结果  
+    printf("%d 和 %d 的最大公约数是: %d\n", num1, num2, gcd(num1, num2));
+
+    return 0;
+}
+
+
+
 
 
