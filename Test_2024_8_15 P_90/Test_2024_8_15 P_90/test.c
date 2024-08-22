e9ececf9b0e6ac63f4cc9c28f6c74a74ba689f98
Practice <hcxiaoling@163.com>
2024-08-22 23:42:33 +0800

    2024_8_22学习资料

--- Test_2024_8_15 P_90/Test_2024_8_15 P_90/test.c
+++ Test_2024_8_15 P_90/Test_2024_8_15 P_90/test.c
@@ -230,16 +230,80 @@
 //}
 
 
+
+//int main() 
+//{
+//    int line = 0;
+//    scanf("%d", &line);
+//    int i = 0;
+//    for (i = 0; i < line; i++)
+//    {
+//        for (int j = 0; j < line - 1 - i; j++)
+//        {
+//            printf(" ");
+//        }
+//        for (int j = 0; j < 2 * i + 1; j++)
+//        {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//
+//    for (i = 0; i < line - 1; i++)
+//    {
+//        for (int j = 0; j <= i; j++)
+//        {
+//            printf(" ");
+//        }
+//        for (int j = 0; j < 2*(line - 1 - i) - 1; j++)
+//        {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//
+//
+//    return 0;
+//}
+
+
+#include <stdio.h>
+
 int main()
 {
-	int line = 0;
-	scanf("%d", &line);
+    char a = 0;
+    scanf("%c", &a);
 
-	int i = 0;
+    int n = 7;
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n - 1 - i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j < 2 * i + 1; j++)
+        {
+            printf("%c", a);
+        }
+        printf("\n");
+    }
 
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < i + 1; j++)
+        {
+            printf(" ");
+        }
+        for (j = 0; j < 2 * (n - 1 - i) - 1 ; j++)
+        {
+            printf("%c", a); 
+        }
+        printf("\n");
+    }
 
-	return 0;
-
+    return 0;
 }
 
 
