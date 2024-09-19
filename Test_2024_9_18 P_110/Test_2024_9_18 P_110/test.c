7e3b9daed6534b84b11953665fc6367f2e77c8b9
Practice <hcxiaoling@163.com>
2024-09-19 18:37:04 +0800

    2024_9_19学习资料

---
+++ Test_2024_9_18 P_110/Test_2024_9_18 P_110/test.c
@@ -0,0 +1,143 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//int main()
+//{
+//	//指针数组
+//	int* arr[4];
+//	char* ch[5];
+//	int** p = arr;//arr数组名 = 首元素地址 = int*类型的地址，也就是一级指针，所以用二级指针p来存
+//
+//	//数组指针
+//	int arr2[5];
+//	int(*pa)[5] = &arr2;
+//
+//	char* arr3[6];
+//	char* (*p3)[6] = &arr3;
+//
+//
+//	return 0;
+//}
+
+
+
+//
+//int test(const char* str)
+//{
+//	printf("test()\n");
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//函数指针 - 也是一种指针，是指向函数的指针
+//	printf("%p\n", test);
+//	printf("%p\n", &test);
+//
+//	int (*pf)(const char*) = &test;
+//	(*pf)("abc");
+//	test("abc");
+//	pf("abc");
+//
+//	return 0;
+//}
+
+
+////typedef unsigned int uint;
+//typedef void(*pf_t)(int);//把void(*)(int)类型重命名为pf_t
+//
+//
+//
+//int main()
+//{
+//	(*(void (*)())0)();//见P105
+//
+//	void(*signal(int, void(*)(int)))(int);
+//	pf_t signal(int, pf_t);
+//
+//
+//	return 0;
+//}
+
+
+
+
+////函数指针的用途
+////写一个计算器
+////加法、减法、乘法、除法
+//
+//void menu()
+//{
+//	printf("***********************\n");
+//	printf("****1. add   2. sub****\n");
+//	printf("****3. mul   4. div****\n");
+//	printf("****    0. exit    ****\n");
+//	printf("***********************\n");
+//}
+//
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+//void calc(int(*pf)(int, int))
+//{
+//	int x = 0;
+//	int y = 0;
+//	int ret = 0;
+//	printf("请输入两个操作数\n");
+//	scanf("%d %d", &x, &y);
+//	ret = pf(x, y);//回调函数
+//	printf("%d\n", ret);
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//
+//	do
+//	{
+//		menu();
+//		printf("请选择\n");
+//		scanf("%d", &input);
+//
+//		switch (input)
+//		{
+//		case 1:
+//			calc(Add);
+//			break;
+//		case 2:
+//			calc(Sub);
+//			break;
+//		case 3:
+//			calc(Mul);
+//			break;
+//		case 4:
+//			calc(Div);
+//			break;
+//		case 0:
+//			printf("退出计算器\n");
+//			break;
+//
+//		default:
+//			printf("选择错误\n");
+//			break;
+//		}
+//
+//	} while (input);
+//
+//	return 0;
+//}
---
+++ 比特直播课/Test_2024_9_18/Test_2024_9_18/test.c
@@ -0,0 +1,70 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//int main()
+//{
+//	int a = 10000;
+//	//00000000 00000000 0010 0111 0001 0000
+//	//00       00       2    7    1    0
+//	FILE * pf = fopen("test.txt", "wb");
+//	fwrite(&a, 4, 1, pf);//⼆进制的形式写到⽂件中
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	FILE* pf = fopen("data.txt", "w");
+//	//相对路径
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//
+//	//打开成功
+//	//写文件
+//
+//
+//	//关闭文件
+//	fclose(pf);
+//	//pf 置为空指针，防止其变成野指针
+//	pf == NULL;
+//
+//
+//	return 0;
+//
+//}
+
+int main()
+{
+	FILE * pf = fopen("data.txt", "w");
+	if (pf == NULL)
+	{
+		perror("fopen");
+		return 1;
+	}
+	//写文件
+	//fputc('x', pf);
+	//fputc('y', pf);
+	//fputc('z', pf);
+	char c = 0;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		fputc(c, stdout);
+	}
+
+
+
+	fclose(pf);
+	pf == NULL;
+
+	return 0;
+}
