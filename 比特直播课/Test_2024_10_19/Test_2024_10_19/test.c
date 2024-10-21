908f58ac6ab96d1c47ff2f6320962a012b551d0a
Practice <hcxiaoling@163.com>
2024-10-21 15:50:02 +0800

    2024_10_21学习资料

--- 比特直播课/Test_2024_10_19/Test_2024_10_19/test.c
+++ 比特直播课/Test_2024_10_19/Test_2024_10_19/test.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
@@ -161,31 +162,246 @@
 //	return 0;
 //}
 
-void test()
-{
-	int* p = (int*)malloc(10 * sizeof(int));
-	if (p == NULL)
-	{
-		perror("malloc");
-		return 1;
-	}
+//void test()
+//{
+//	int* p = (int*)malloc(10 * sizeof(int));
+//	if (p == NULL)
+//	{
+//		perror("malloc");
+//		return 1;
+//	}
+//
+//	if (1)
+//	{
+//		return;
+//	}
+//
+//
+//	free(p);
+//	p = NULL;
+//}
+//
+//int main()
+//{
+//	test();
+//
+//	return 0;
+//}
 
-	if (1)
-	{
-		return;
-	}
 
 
-	free(p);
-	p = NULL;
-}
+//void GetMemory(char** p)
+//{
+//	*p = (char*)malloc(100);
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	GetMemory(&str);
+//	strcpy(str, "hello world");
+//	printf("%s", str);
+//	free(str);
+//	str = NULL;
+//}
+//
+//int main()
+//{
+//	Test();
+//
+//	return 0;
+//}
 
-int main()
-{
-	test();
 
-	return 0;
-}
+//char* GetMemory()
+//{
+//	char* p = (char*)malloc(100);
+//	return p;
+//}
+//void Test(void)
+//{
+//	char* str = NULL;
+//	str = GetMemory();
+//	strcpy(str, "hello world");
+//	printf("%s", str);
+//	free(str);
+//	str = NULL;
+//}
+//
+//int main()
+//{
+//	Test();
+//
+//	return 0;
+//}
+
+
+
+
+//char* GetMemory(void)
+//{
+//	char p[] = "hello world";
+//	return p;
+//}
+//
+//void Test(void)
+//{
+//	char* str = NULL;
+//	str = GetMemory();
+//	printf(str);
+//}
+//
+//int main()
+//{
+//	Test();
+//
+//	return 0;
+//}
+
+
+
+
+//void GetMemory(char** p, int num)
+//{
+//	*p = (char*)malloc(num);
+//}
+//
+//void Test(void)
+//{
+//	char* str = NULL;
+//	GetMemory(&str, 100);
+//	strcpy(str, "hello");
+//	printf(str);
+//	free(str);
+//}
+//
+//int main()
+//{
+//	Test();
+//
+//	return 0;
+//}
+
+
+
+//void Test(void)
+//{
+//	char* str = (char*)malloc(100);
+//	strcpy(str, "hello");
+//	free(str);
+//	str = NULL;
+//	if (str != NULL)
+//	{
+//		strcpy(str, "world");
+//		printf(str);
+//	}
+//}
+//
+//int main()
+//{
+//	Test();
+//
+//	return 0;
+//}
+
+
+//struct S
+//{
+//	int n;
+//	int arr[];
+//};
+//
+//int main()
+//{
+//	//struct S s;//不会这样写
+//	//会这样写
+//	struct S* ps = (struct S*)malloc(sizeof(struct S) + 5 * (sizeof(int)));
+//	ps->n = 100;
+//	for (int i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i + 1;
+//	}
+//	struct S* tmp = (struct S*)realloc(ps, sizeof(struct S) + 40);
+//	if (tmp == NULL)
+//	{
+//		perror("realloc");
+//		return 1;
+//	}
+//	ps = tmp;
+//	for (int i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i + 1;
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//
+//	free(ps);
+//	ps = NULL;
+//	
+//	//printf("%zd\n", sizeof(struct S));
+//}
+
+
+//struct S2
+//{
+//	int n;
+//	int* arr;
+//};
+//
+//int main()
+//{
+//	struct S2* ps = (struct S2*)malloc(sizeof(struct S2));
+//	if (ps == NULL)
+//	{
+//		perror("malloc-1");
+//		return 1;
+//	}
+//	ps->n = 100;
+//	ps->arr = (int*)malloc(5 * sizeof(int));
+//	if (ps->arr == NULL)
+//	{
+//		perror("malloc-2");
+//		return 1;
+//	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i + 1;
+//	}
+//	int* ptr = (int*)realloc(ps->arr, 10 * sizeof(int));
+//	if (ptr == NULL)
+//	{
+//		perror("realloc");
+//		return 1;
+//	}
+//	else
+//	{
+//		ps->arr = ptr;
+//	}
+//	for (int i = 5; i < 10; i++)
+//	{
+//		ps->arr[i] = i + 1;
+//	}
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//
+//	free(ps->arr);
+//	ps->arr = NULL;
+//
+//	free(ps);
+//	ps = NULL;
+//
+//
+//	return 0;
+//}
+
+
+
+
+
+
 
 
 
--- 比特直播课/Test_2024_9_18/Test_2024_9_18/test.c
+++ 比特直播课/Test_2024_9_18/Test_2024_9_18/test.c
@@ -6,17 +6,17 @@
 #include <stdlib.h>
 
 
-//int main()
-//{
-//	int a = 10000;
-//	//00000000 00000000 0010 0111 0001 0000
-//	//00       00       2    7    1    0
-//	FILE * pf = fopen("test.txt", "wb");
-//	fwrite(&a, 4, 1, pf);//⼆进制的形式写到⽂件中
-//	fclose(pf);
-//	pf = NULL;
-//	return 0;
-//}
+int main()
+{
+	int a = 10000;
+	//00000000 00000000 0010 0111 0001 0000
+	//00       00       2    7    1    0
+	FILE * pf = fopen("test.txt", "wb");
+	fwrite(&a, 4, 1, pf);//⼆进制的形式写到⽂件中
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
 
 
 //int main()
@@ -43,28 +43,28 @@
 //
 //}
 
-int main()
-{
-	FILE * pf = fopen("data.txt", "w");
-	if (pf == NULL)
-	{
-		perror("fopen");
-		return 1;
-	}
-	//写文件
-	//fputc('x', pf);
-	//fputc('y', pf);
-	//fputc('z', pf);
-	char c = 0;
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		fputc(c, stdout);
-	}
-
-
-
-	fclose(pf);
-	pf == NULL;
-
-	return 0;
-}
+//int main()
+//{
+//	FILE * pf = fopen("data.txt", "w");
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	//写文件
+//	//fputc('x', pf);
+//	//fputc('y', pf);
+//	//fputc('z', pf);
+//	char c = 0;
+//	for (c = 'a'; c <= 'z'; c++)
+//	{
+//		fputc(c, stdout);
+//	}
+//
+//
+//
+//	fclose(pf);
+//	pf == NULL;
+//
+//	return 0;
+//}
