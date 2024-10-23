68d43d79e98959bbce08f66ddfcdcdb7fab6104e
Practice <hcxiaoling@163.com>
2024-10-23 18:37:56 +0800

    2024_10_23学习资料

--- 比特直播课/数据结构/SList_2024_10_10/SList_2024_10_10/SList.c
+++ 比特直播课/数据结构/SList_2024_10_10/SList_2024_10_10/SList.c
@@ -1,17 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "SList.h"
 
-// 单链表打印
-void SLTprint(SLTNode* phead)
-{
-	SLTNode* pcur = phead;
-	while (pcur != NULL)
-	{
-		printf("%d -> ", pcur->data);
-		pcur = pcur->next;
-	}
-	printf("NULL\n");
-}
+
 
 // 动态申请一个节点
 SLTNode* SLTBuyNode(SLTDataType x)
@@ -28,16 +18,28 @@ SLTNode* SLTBuyNode(SLTDataType x)
 	return node;
 }
 
+// 单链表打印
+void SLTprint(SLTNode* phead)
+{
+	SLTNode* pcur = phead;
+	while (pcur != NULL)
+	{
+		printf("%d -> ", pcur->data);
+		pcur = pcur->next;
+	}
+	printf("NULL\n");
+}
+
 // 单链表尾插
 void SLTPushBack(SLTNode** pphead, SLTDataType x)
 {
 	assert(pphead);
 	SLTNode* newnode = SLTBuyNode(x);
-	if (*pphead == NULL)
+	if (*pphead == NULL)//链表为空时
 	{
 		*pphead = newnode;
 	}
-	else
+	else//链表非空时
 	{
 		SLTNode* ptail = *pphead;
 		while (ptail->next)//等价于 ptail->next != NULL
@@ -91,6 +93,110 @@ void SLTPopFront(SLTNode** pphead)
 	SLTNode* next = (*pphead)->next;
 	free(*pphead);
 	*pphead = next;
-	
 }
+
+// 单链表查找
+SLTNode* SListFind(SLTNode* plist, SLTDataType x)
+{
+	assert(plist);
+	SLTNode* find = plist;
+	while (find)
+	{
+		if (find->data == x)
+		{
+			return find;
+		}
+		find = find->next;
+	}
+	return NULL;
+}
+
+// 单链表在pos位置之后插入x
+void SListInsertAfter(SLTNode* pos, SLTDataType x)
+{
+	assert(pos);// 检查 pos 是否为 NULL
+
+	SLTNode* newnode = SLTBuyNode(x);
+	if (newnode == NULL)
+	{
+		perror("SLTBuyNode failed");
+		return;
+	}
+
+	newnode->next = pos->next;
+	pos->next = newnode;       
+}
+
+// 单链表删除pos位置之后的值
+void SListEraseAfter(SLTNode* pos)
+{
+	assert(pos && pos->next);
+	if (pos->next->next == NULL)
+	{
+
+	}
+
+}
+
+
+
+
+
+
+
+
+// 在pos的前面插入
+void SLTInsert(SLTNode** pphead, SLTNode* pos, SLTDataType x)
+{
+	assert(pphead && pos);
+	if (pos == *pphead)
+	{
+		SLTPushFront(pphead, x);
+		return;
+	}
+
+	SLTNode* newnode = SLTBuyNode(x);
+	if (newnode == NULL)
+	{
+		perror("SLTBuyNode failed");
+		return;
+	}
+
+	SLTNode* prev = *pphead;
+	while (prev->next != pos)
+	{
+		prev = prev->next;
+	}
+	newnode->next = pos;
+	prev->next = newnode;
+}
+
+// 删除pos位置
+void SLTErase(SLTNode** pphead, SLTNode* pos)
+{
+	assert(pphead && pos);
+	SLTNode* pcur = *pphead;
+	
+	if (pcur == pos)// 如果要删除的是头节点
+	{
+		*pphead = pcur->next; // 更新头节点
+		free(pcur);           // 释放头节点
+		return;
+	}
+
+	SLTNode* prev = NULL;
+	while (pcur != NULL && pcur != pos)// 查找 pos 的前驱节点
+	{
+		prev = pcur;
+		pcur = pcur->next;
+	}
+
+	if (pcur == pos)// 如果找到了要删除的节点
+	{
+		prev->next = pcur->next; // 跳过要删除的节点
+		free(pcur);              // 释放 pos
+	}
+}
+
+
 
--- 比特直播课/数据结构/SList_2024_10_10/SList_2024_10_10/SList.h
+++ 比特直播课/数据结构/SList_2024_10_10/SList_2024_10_10/SList.h
@@ -3,25 +3,49 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef int SLTDataType;
+typedef int SLTDataType;//定义结构体所储存data数据的类型
 
-typedef struct SListNode
+typedef struct SLTNode//定义一个结构体SLTNode
 {
-	SLTDataType data;
-	struct SListNode* next;
+	SLTDataType data;//存储数据
+	struct SLTNode* next;//链式储存next节点的地址
 }SLTNode;
+
+
+// 动态申请一个节点
+SLTNode* SLTBuyNode(SLTDataType x);
 
 // 单链表打印
 void SLTprint(SLTNode* phead);
-// 动态申请一个节点
-SLTNode* SLTBuyNode(SLTDataType x);
+
 //尾插
 void SLTPushBack(SLTNode** pphead, SLTDataType x);
+
 //头插
 void SLTPushFront(SLTNode** pphead, SLTDataType x);
+
 //尾删
 void SLTPopBack(SLTNode** pphead);
+
 //头删
 void SLTPopFront(SLTNode** pphead);
 
+// 单链表查找
+SLTNode* SListFind(SLTNode* plist, SLTDataType x);
 
+// 单链表在pos位置之后插入x
+// 分析思考为什么不在pos位置之前插入？
+void SListInsertAfter(SLTNode* pos, SLTDataType x);
+
+// 单链表删除pos位置之后的值
+// 分析思考为什么不删除pos位置？
+void SListEraseAfter(SLTNode* pos);
+
+// 在pos的前面插入
+void SLTInsert(SLTNode** pphead, SLTNode* pos, SLTDataType x);
+
+// 删除pos位置
+void SLTErase(SLTNode** pphead, SLTNode* pos);
+
+//销毁
+void SLTDestroy(SLTNode** pphead);
