a0a22c23615ffb0ef71b027b6c5c229ea84fe097
Practice <hcxiaoling@163.com>
2024-11-07 17:58:38 +0800

    2024_11_7学习资料

---
+++ 比特直播课/比特直播课代码-C++/Test_2024_11_6/Test_2024_11_6/test.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+//// test.cpp
+//#include<stdio.h>
+//int main()
+//{
+//	printf("hello world\n");
+//	return 0;
+//}
+
+
+// test.cpp
+// 这⾥的std cout等我们都看不懂，没关系，下⾯我们会依次讲解
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "hello world\n" << endl;
+
+    return 0;
+}
