1a410cd51d4dc92ef919158a2b0edf0e24e5b0f0
CoderDavid <hcxiaoling@163.com>
2025-03-11 06:24:08 +0800

    2025_3_10学习资料

--- 比特直播课/比特直播课代码-C++/Test_2025_1_19/Test_2025_1_19/stack.h
+++ 比特直播课/比特直播课代码-C++/Test_2025_1_19/Test_2025_1_19/stack.h
@@ -1,19 +1,42 @@
 #pragma once
 
+#include <vector>
+#include <list>
+
+using namespace std;
+
 namespace LDW
 {
 	//stack<int, vector<int>>
 	//stack<int, list<int>>
-	template<class T, class Container>// 写模板的时候采用Container容器参数，就能控制是数组栈还是链表栈
+	template<class T, class Container = vector<T>>// 写模板的时候采用Container容器参数，就能控制是数组栈还是链表栈
 	class stack
 	{
 	public:
 		void push(const T& x)
 		{
-			_con.push_back(x);// push就相当于在栈顶插入数据呗
+			_con.push_back(x);// push在栈顶插入数据就相当于容器的尾插呗
 		}
 
+		void pop()
+		{
+			_con.pop_back();// pop出栈就相当于容器的尾删呗
+		}
 
+		const T& top() const
+		{
+			return _con.back();// top就是返回栈顶元素，就是容器的尾部元素，back这个接口就起作用了
+		}
+
+		size_t size() const
+		{
+			return _con.size();// size就是容器的size
+		}
+
+		bool empty() const
+		{
+			return _con.empty();// empty就是容器的empty
+		}
 
 	private:
 		Container _con;
--- 比特直播课/比特直播课代码-C++/Test_2025_1_19/Test_2025_1_19/test.cpp
+++ 比特直播课/比特直播课代码-C++/Test_2025_1_19/Test_2025_1_19/test.cpp
@@ -1,7 +1,27 @@
 
 
+
+
+#include <iostream>       // std::cout
+#include <stack>          // std::stack
+
+
+#include "stack.h"
+
 int main()
 {
-	
+	LDW::stack<int, list<int>> mystack;
+
+	for (int i = 0; i < 5; ++i) mystack.push(i);
+
+	std::cout << "Popping out elements...";
+	while (!mystack.empty())
+	{
+		std::cout << ' ' << mystack.top();
+		mystack.pop();
+	}
+
+	std::cout << '\n';
+
 	return 0;
 }
