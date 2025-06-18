8ecb02738d9f2b23c39999c1085d4dfea32a88fc
CoderDavid <hcxiaoling@163.com>
2025-06-18 23:03:10 +0800

    2025_6_18学习资料

--- 比特直播课/比特直播课代码-C++/Test_2025_3_18 HashTable/Test_2025_3_18 HashTable/HashTable.h
+++ 比特直播课/比特直播课代码-C++/Test_2025_3_18 HashTable/Test_2025_3_18 HashTable/HashTable.h
@@ -1,78 +1,224 @@
 #pragma once
 #include<vector>
 #include<utility>
+#include <algorithm>
 using namespace std;
 
-enum State
+// 哈希表扩容用的素数表
+// 这些接近2的幂次方的素数可以减少哈希冲突
+static const int kNumPrimes = 28;  // 素数表大小
+static const unsigned long kPrimeList[kNumPrimes] = 
 {
-    EXIST,   // 表示该位置有数据
-    EMPTY,   // 表示该位置为空
-    DELETE   // 表示该位置数据已被删除
+    // 从53开始，每个素数大约为前一个的2倍
+    53,         97,         193,        389,        769,
+    1543,       3079,       6151,       12289,      24593,
+    49157,      98317,      196613,     393241,     786433,
+    1572869,    3145739,    6291469,    12582917,   25165843,
+    50331653,   100663319,  201326611,  402653189,  805306457,
+    1610612741, 3221225473, 4294967291  // 最后一个素数是2^32-5
 };
 
-template<class K, class V>
-struct HashData
+ /**
+ * 获取不小于n的最小素数
+ * @param n 输入数值
+ * @return 素数表中≥n的最小素数，如果n超过最大值则返回最后一个素数
+ */
+inline unsigned long NextPrime(unsigned long n) 
 {
-    pair<K, V> _kv;       // 存储键值对
-    State _state = EMPTY;  // 初始状态为EMPTY //线性探测情况下，映射会有自然的错位，如果删除某些位置的数据，再探测，以空为条件结束，就会产生明明在表内却探测不到的情况
-    //所以需要添加一个状态，当状态为“被删除”时，依然会向后找，只有同时“空”并且“K值==对应位置”才确认了，哦，之前确实没有这个数，见3_15 map和set的封装 02:57:00 部分
-};
+    const unsigned long* first = kPrimeList;          // 素数表起始位置
+    const unsigned long* last = kPrimeList + kNumPrimes;  // 结束位置(尾后指针)
+    const unsigned long* pos = std::lower_bound(first, last, n);  // 二分查找
 
-template<class K, class V>
-class HashTable
+    // 如果n比所有素数都大，返回最后一个素数；否则返回找到的位置的值
+    return pos == last ? *(last - 1) : *pos;
+}
+
+
+template<class K>
+struct HashFunc
 {
-public:
-    HashTable()
-        :_n(0)
-        ,_tables(11)
-    {}
-
-    bool Insert(const pair<K, V>& kv)
+    size_t operator()(const K& key)
     {
-        //一般负载因子到0.7左右就需要扩容了，不能等满了再扩
-        if (_n / _tables.size() >= 0.7)
-        {
-
-        }
-
-        size_t hash0 = kv.first % _tables.size();//这里的size是11哦，先前指定了vector<HashData<K, V>> _tables默认给了个11
-        size_t hashi = hash0;
-        size_t i = 1;
-        //线性探测
-        while (_tables[hashi]._state == EXIST)
-        {
-            hashi = (hashi + i) % _tables.size();
-            ++i;
-        }
-        _tables[hashi]._kv = kv;
-        _tables[hashi]._state = EXIST;
-        ++_n;
-
-        return true;
+        return (size_t)key;
     }
+};
 
-    HashData<K, V>* Find(const K& key)
+template<>
+struct HashFunc<string>
+{
+    size_t operator()(const string& key)
     {
-
-        size_t hash0 = kv.first % _tables.size();
-        size_t hashi = hash0;
-        size_t i = 1;
-        //线性探测
-        while (_tables[hashi]._state != EMPTY)
+        size_t hashi = 0;
+        for (auto ch : key)
         {
-            if (_tables[hashi]._kv.firstt == key)
+            hashi *= 131;
+            hashi += ch;
+        }
+        return hashi;
+    }
+};
+
+
+namespace open_address
+{
+    enum State
+    {
+        EXIST,   // 表示该位置有数据
+        EMPTY,   // 表示该位置为空
+        DELETE   // 表示该位置数据已被删除
+    };
+
+    template<class K, class V>
+    struct HashData
+    {
+        pair<K, V> _kv;       // 存储键值对
+        State _state = EMPTY;  // 初始状态为EMPTY //线性探测情况下，映射会有自然的错位，如果删除某些位置的数据，再探测，以空为条件结束，就会产生明明在表内却探测不到的情况
+        //所以需要添加一个状态，当状态为“被删除”时，依然会向后找，只有同时“空”并且“K值==对应位置”才确认了，哦，之前确实没有这个数，见3_15 map和set的封装 02:57:00 部分
+    };
+
+    template<class K, class V, class Hash = HashFunc<K>>
+    class HashTable
+    {
+    public:
+        HashTable(size_t size = NextPrime(0))
+            :_n(0)
+            , _tables(size)
+        {
+        }
+
+        bool Insert(const pair<K, V>& kv)
+        {
+            //一般负载因子到0.7左右就需要扩容了，不能等满了再扩
+            if ((double)_n / (double)_tables.size() >= 0.7)
             {
-                return hashi;
+                //vector<HashData<K, V>> _newtables(_tables.size() * 2);
+                //for (size_t i = 0; i < _tables.size(); i++)
+                //{
+                //    if (_tables[i].state == EXIST)
+                //    {
+                //        //...手动遍历法
+                //    }
+                //}
+
+                //现代写法 复用，我不自己写，你写了，我换过来
+                //HashTable<K, V> newHT(_tables.size() * 2);
+                HashTable<K, V, Hash> newHT(NextPrime(_tables.size() + 1));
+                for (size_t i = 0; i < _tables.size(); i++)
+                {
+                    if (_tables[i]._state == EXIST)
+                    {
+                        newHT.Insert(_tables[i]._kv);
+                    }
+                }
+                _tables.swap(newHT._tables);
             }
+            Hash hs;//用仿函数处理string or double类型无法比较的问题
+            size_t hash0 = hs(kv.first) % _tables.size();//这里的size是11哦，先前指定了vector<HashData<K, V>> _tables默认给了个11
+            size_t hashi = hash0;
+            size_t i = 1;
+            //线性探测
+            while (_tables[hashi]._state == EXIST)
+            {
+                hashi = (hashi + i) % _tables.size();
+                ++i;
+            }
+            _tables[hashi]._kv = kv;
+            _tables[hashi]._state = EXIST;
+            ++_n;
 
-            hashi = (hashi + i) % _tables.size();
-            ++i;
+            return true;
         }
-        return nullptr;
-    }
 
+        HashData<K, V>* Find(const K& key)
+        {
+            Hash hs;
+            size_t hash0 = hs(key) % _tables.size();
+            size_t hashi = hash0;
+            size_t i = 1;
+            //线性探测
+            while (_tables[hashi]._state != EMPTY)
+            {
+                if (_tables[hashi]._kv.first == key && _tables[hashi]._state != DELETE)
+                {
+                    return &_tables[hashi];
+                }
 
-private:
-    vector<HashData<K, V>> _tables;  // 存储哈希节点的数组
-    size_t _n = 0;                  // 表中实际存储的数据个数
-};
+                hashi = (hashi + i) % _tables.size();
+                ++i;
+            }
+            return nullptr;
+        }
+
+        bool Erase(const K& key)
+        {
+            HashData<K, V>* ret = Find(key);
+            if (ret)
+            {
+                ret->_state = DELETE;
+                return true;
+            }
+            else return false;
+        }
+
+    private:
+        vector<HashData<K, V>> _tables;  // 存储哈希节点的数组
+        size_t _n = 0;                  // 表中实际存储的数据个数
+    };
+}
+
+namespace hash_bucket
+{
+    template<class K, class V>
+    struct HashNode
+    {
+        pair<K, V> _kv;
+        HashNode<K, V>* _next;
+
+        HashNode(const pair<K, V>& kv)
+            :_kv(kv)
+            , _next(nullptr)
+        {
+        }
+    };
+
+    template<class K, class V>
+    class HashTable
+    {
+        typedef HashNode<K, V> Node;
+    public:
+        HashTable(size_t size = NextPrime(0))
+            :_tables(size, nullptr)
+        {}
+
+        bool Insert(const pair<K, V>& kv)
+        {
+            //扩容
+            //负载因子到1，再扩容
+            if (_n == _tables.size())
+            {
+                HashTable<K, V> newHT(NextPrime(_tables.size() + 1));
+                for (size_t i = 0; i < _tables.size(); i++)
+                {
+                    Node* cur = _tables[i];
+                    while (cur)
+                    {
+                        newHT.Insert(cur->_kv);
+                        cur = cur->_next;
+                    }
+                }
+                _tables.swap(newHT._tables);
+            }
+            //Hash hs;
+            size_t hashi = hs(kv.first) % _tables.size();
+            Node* newnode = new Node(kv);
+            //选用头插，免得去找尾又
+            newnode->_next = _tables[hashi];
+            _tables[hashi] = newnode;
+            ++_n;
+        }
+    private:
+        //vector<list<pair<K, V>>> _tables;//这是一种实现哈希桶的方式，但是不太利于之后封装unordered_xxx实现迭代器
+        vector<Node*> _tables;// 所以用原生一些的结构，指针数组
+        size_t _n = 0;// 存了多少有效数据
+    };
+}
--- 比特直播课/比特直播课代码-C++/Test_2025_3_18 HashTable/Test_2025_3_18 HashTable/test.cpp
+++ 比特直播课/比特直播课代码-C++/Test_2025_3_18 HashTable/Test_2025_3_18 HashTable/test.cpp
@@ -1,25 +1,73 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include<iostream>
+#include<unordered_map>
 #include"HashTable.h"
 
 void testHT()
 {
 	int a[] = { 19,30,5,36,13,20,21,12 };
-	HashTable<int, int> ht;
+	open_address::HashTable<int, int> ht;
 	for (auto e : a)
 	{
 		ht.Insert({e, e});
 	}
+	ht.Insert({ 34, 34 });
+	for (int i = 0; i < 54; ++i)
+	{
+		ht.Insert({ rand(), 1});
+	}
+	
+
+
+	cout << ht.Find(20) << endl;
+	cout << ht.Find(9) << endl;
+
+	ht.Erase(30);
+	cout << ht.Find(-20) << endl;
+	cout << ht.Find(9) << endl;
+	cout << ht.Find(30) << endl;
+
 }
 
+struct Date
+{
+	int _year;
+	int _month;
+	int _day;
+};
 
 
+void testHT2()
+{
+	//HashTable<string, string, HashFuncString> dict;
+	open_address::HashTable<string, string> dict;
+	dict.Insert({ "sort", "排序"});
+	dict.Insert({ "string", "字符串"});
+
+	open_address::HashTable<double, int> ht;
+	ht.Insert({ 1.23, 1 });
+
+	unordered_map<string, string> stddict;//库里的string不用写仿函数是因为特化处理了
+	stddict.insert({ "sort", "排序" });
+	stddict.insert({ "string", "字符串" });
+
+	//unordered_map<Date, string> m2; //比如自定义这种日期类要做K键值就必须自己写仿函数
+
+}
 
 
 int main()
 {
+	//testHT();
 	testHT();
+	testHT2();
 
+	HashFunc<string> hf;
+	cout << hf("abcd") << endl;
+	cout << hf("bcad") << endl;
+	cout << hf("aadd") << endl;//那像这种虽然顺序不一样，但总+值相同的字符串怎么处理最好呢？
+	//有很多解决方法 比如BKDR hash
 
 	return 0;
 }
